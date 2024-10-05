7537f9260da2ef49212530639632f2ba135b28f9
Youssef-001 <youssefsamir432@gmail.com>
2024-10-05 08:52:28 +0300

    Add Home link in cart page

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,11 +1,32 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
+import Header from "./Header";
+import { House } from "lucide-react";
+import styled from "styled-components";
+
+let HomeButton = styled.button`
+  all: unset;
+  margin-left: auto;
+  margin-right: 1rem;
+`;
 
 function CartPage() {
   const location = useLocation();
   const { cart } = location.state || {}; // Handle case if state is undefined
   console.log("here", cart);
 
-  if (cart) return <>cart</>;
+  if (cart) {
+    return (
+      <>
+        <Header>
+          <HomeButton>
+            <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+              <House size="48px" />
+            </Link>
+          </HomeButton>
+        </Header>
+      </>
+    );
+  }
 }
 
 export default CartPage;
