3d9bb55028dfe6b3b0a141a5c21e8d0cb57f0da9
Youssef-001 <youssefsamir432@gmail.com>
2024-10-17 14:36:36 +0300

    Edit vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom", // Ensures Vitest uses the jsdom environment
-    globals: true, // Allows the use of global methods like `expect`
-  },
 });
